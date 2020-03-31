9cbc8add345a12d71b41538e876ad34e831ac666
Dennis Dunn <dennis.dunn@outlook.com>
2020-03-30 23:14:15 -0700

    The audio streaming is set up.

--- rtld/entrypoint.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-rtl_fm -f 102900000 | sox -traw -r24k -es -b16 -c1 -V1 - -tmp3 - | socat -u - TCP-LISTEN:9001,fork
---
+++ sdrd/entrypoint.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+#rtl_fm -f 102900000 |  socat -u - TCP-LISTEN:9001,fork
+#rtl_fm -f 102900000 | sox -traw -r24k -es -b16 -c1 -V1 - -tmp3 - | socat -u - TCP-LISTEN:9001,fork
+rtl_fm -f 102900000 | ezstream -c /etc/ezstream.xml
