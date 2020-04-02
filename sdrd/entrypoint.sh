0656eb904a21ea04dc06ff02d0ad6ff6150fe1cb
Dennis Dunn <ansofive@gmail.com>
2020-04-01 18:17:43 -0700

    Is alpine causing my problems? Probably not.

--- sdrd/entrypoint.sh
+++ sdrd/entrypoint.sh
@@ -2,4 +2,4 @@
 
 #rtl_fm -f 102900000 |  socat -u - TCP-LISTEN:9001,fork
 #rtl_fm -f 102900000 | sox -traw -r24k -es -b16 -c1 -V1 - -tmp3 - | socat -u - TCP-LISTEN:9001,fork
-rtl_fm -f 89900000 -W | ezstream -c /data/ezstream.xml
+rtl_fm -f 89.9M -M wbfm | ices2 ices.xml
