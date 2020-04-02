29ebea0eb2390b77c9584e5318af4b429cc3e353
Dennis Dunn <ansofive@gmail.com>
2020-04-01 19:30:50 -0700

    Changed from alpine to debian, problem solved.

--- sdrd/entrypoint.sh
+++ sdrd/entrypoint.sh
@@ -2,4 +2,4 @@
 
 #rtl_fm -f 102900000 |  socat -u - TCP-LISTEN:9001,fork
 #rtl_fm -f 102900000 | sox -traw -r24k -es -b16 -c1 -V1 - -tmp3 - | socat -u - TCP-LISTEN:9001,fork
-rtl_fm -f 89.9M -M wbfm | ices2 ices.xml
+rtl_fm -f 89.9M -W | ices2 /data/ices.xml
